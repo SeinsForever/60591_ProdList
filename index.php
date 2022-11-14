5ad258ca2e450990aeba7d7d0b3067ebedb52975
poddu <ivan_poddubnyy01@mail.ru>
2022-11-15 00:02:43 +0500

    Стартовый проект HTML Академия

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require('dbdconnect.php');
+require('auth.php');
+require('components/header.php');
+require('components/company_info.php');
+require('components/message.php');
+require('components/footer.php');
