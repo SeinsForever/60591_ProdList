8ec19370640d7907742613f86040e22d8a32e232
poddu <ivan_poddubnyy01@mail.ru>
2022-11-15 20:32:49 +0500

    Стартовый проект HTML Академия

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/vendor/autoload.php'; //загрузка всех установленных библиотек
+use dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
+if (file_exists(__DIR__."/.env"))
+{
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load(); //все параметры окружения помещаются в массив $_ENV
+}
